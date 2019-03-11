e45bad8bdf2c8eaaa465247bf20fb42de886a05b
Mickael Alliel <mickael@rookout.com>
2019-03-11 21:09:56 +0200

    permit all unauthorized requests

--- src/main/java/com/demo/SecurityConfig.java
+++ src/main/java/com/demo/SecurityConfig.java
@@ -29,9 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and()
-                .csrf().disable()
-                .httpBasic();
+        http.authorizeRequests().anyRequest().permitAll().and()
+                .csrf().disable();
     }
 
     @Override
